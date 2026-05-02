80025116a289f66bda53806443a853aa3a4c7398
elenekartvelishvili <eleneqartvelishvili61@gmail.com>
2026-05-02 15:54:02 +0400

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Lecture_7_1.Entities;
+using Microsoft.EntityFrameworkCore;
+using DbContext = System.Data.Entity.DbContext;
+
+namespace Lecture_7_1.Context;
+
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer(
+            "Server=DESKTOP-7450GMH;Database=DotNet;Trusted_Connection=True;TrustServerCertificate=True");
+}
